3026bdd1e98f722fa8e015b67eb99b669be398e8
dickymahfudin <dickymahfudin13@gmail.com>
2023-02-01 23:53:29 +0700

    update

--- public/js/datatables.js
+++ public/js/datatables.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
       if (!dism) {
         if (parsUrl == 'bread') {
           const clm = response.columns;
-          response.columns = [clm[0], clm[1], clm[2], clm[4], clm[5], clm[3]];
+          response.columns = [clm[0], clm[1], clm[2], clm[5], clm[4], clm[3]];
         }
         response.columns.push({
           data: 'id',
